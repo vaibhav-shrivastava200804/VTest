a4b818491059c1bdd98091c423c3597ebb8067c1
vaibhav-shrivastava200804 <vaibhav2000804@gmail.com>
2025-04-24 14:35:24 +0530

    project with  deployed backend

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 // Default export
 const config = {
-    API_BASE_URL: "http://localhost:3000/api/questions"  // Replace with actual URL
+    API_BASE_URL: "http://vtestbackend.onrender.com/api/questions"  // Replace with actual URL
 };
 
 export default config;
